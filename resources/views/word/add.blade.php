774deaf8015ef8295d62e1e66d4e82957d598628
1405043-kd <1405043-kd@ugrad.cse.buet.ac.bd>
2018-04-01 23:19:17 +0600

    tag to database done

--- resources/views/word/add.blade.php
+++ resources/views/word/add.blade.php
@@ -7,6 +7,7 @@
         <input type="text" name="name" placeholder="Enter Word Name" >
         <input type="text" name="def" placeholder="Enter Definition" >
         <input type="text" name="sentence_ex" placeholder="Enter Sentence Example" >
+        <input type="text" name="tags" placeholder="Tags">
 
         <input type="submit" name="submit">
     </form>
