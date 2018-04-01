c944b31ed2ff69fcab7f0d3875e5b646797120f6
1405043-kd <1405043-kd@ugrad.cse.buet.ac.bd>
2018-04-02 02:27:15 +0600

    form design primitive

--- resources/views/word/add.blade.php
+++ resources/views/word/add.blade.php
@@ -1,7 +1,14 @@
 @extends('layouts.app')
 
+<link href="//netdna.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//netdna.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+
+
 @section('content')
 
+
+<!--
     <form method="post" action="/words">
         <input name="_token" type="hidden" value="{{ csrf_token() }}"/>
         <input type="text" name="name" placeholder="Enter Word Name" >
@@ -11,3 +18,45 @@
 
         <input type="submit" name="submit">
     </form>
+>
+
+
+
+
+
+
+
+
+
+
+    <!------ Include the above in your HEAD tag ---------->
+
+    <div class="container">
+        <div class="col-md-8">
+            <div class="form-area">
+
+                <form action="/words" method="post" >
+                    <div class="form-group">
+                        <input name="_token" type="hidden" value="{{ csrf_token() }}"/>
+                    </div>
+                    <br style="clear:both">
+                    <h3 style="margin-bottom: 25px; text-align: center;">শব্দ করি যোগ </h3>
+                    <div class="form-group">
+                        <input type="text" class="form-control" name="name" placeholder="শব্দ" required>
+                    </div>
+                    <div class="form-group">
+                        <textarea type="text" class="form-control" name="def" placeholder="ব্যাখ্যা বনাম অর্থ" maxlength="140" rows="6"></textarea>
+                    </div>
+                    <div class="form-group">
+                        <textarea type="text" class="form-control" name="sentence_ex" placeholder="উদাহরণ" maxlength="140" rows="4" required></textarea>
+                    </div>
+                    <div class="form-group">
+                        <input type="text" class="form-control" name="tags" placeholder="উৎস" required>
+                    </div>
+
+
+                    <button type="submit" id="submit" name="submit" class="btn btn-primary pull-right">Submit Form</button>
+                </form>
+            </div>
+        </div>
+    </div>
